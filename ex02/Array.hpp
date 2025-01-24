95b22ac0956495b933ff5c3127bb262b264ebe62
Thomas Parratt <tparratt@c1r3p7.hive.fi>
2025-01-24 14:52:59 +0200

    progressing

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -5,7 +5,30 @@ template<typename T>
 class Array
 {
     private:
-
+        T _arr[];
     public:
-    
+        Array()
+        {
+        
+        }
+        Array(unsigned int n)
+        {
+            for (unsigned int i = 0; i < n; i++)
+            {
+                _arr[i] = int();
+            }
+        }
+        Array(const Array& obj)
+        {
+            _arr = obj._arr;
+        }
+        ~Array() {}
+        Array& operator=(const Array& obj)
+        {
+            if (this != &obj)
+            {
+                _arr = obj._arr;
+            }
+            return (*this);
+        }
 };
---
+++ ex02/main.cpp
@@ -0,0 +1,9 @@
+#include "Array.hpp"
+
+int main(void)
+{
+    Array<int> *array = new Array<int>();
+
+    // int * a = new int(); 
+    // std::cout << *a << std::endl;
+}
