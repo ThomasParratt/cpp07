5b7c68049def219f7a02437a01d1b3479571d8ae
Thomas Parratt <tparratt@c1r3p7.hive.fi>
2025-01-30 10:25:13 +0200

    updated main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,22 +1,5 @@
 #include "template.hpp"
 
-// int main(void)
-// {
-//     int a = 2;
-//     int b = 1;
-
-//     std::cout << "a = " << a << std::endl;
-//     std::cout << "b = " << b << std::endl;
-    
-//     swap(a, b);
-
-//     std::cout << "a = " << a << std::endl;
-//     std::cout << "b = " << b << std::endl;
-
-//     std::cout << "min = " << min(a,b) << std::endl;
-//     std::cout << "max = " << max(a,b) << std::endl;
-// }
-
 int main(void) //from subject
 {
     int a = 2;
