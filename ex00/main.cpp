e677f663dc1f564d7c9d8c9b614a286f560f88ff
Thomas Parratt <tparratt@c2r1p7.hive.fi>
2025-02-04 11:11:28 +0200

    tests added

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,6 +1,6 @@
 #include "template.hpp"
 
-int main(void) //from subject
+int main(void)
 {
     int a = 2;
     int b = 3;
@@ -14,5 +14,18 @@ int main(void) //from subject
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
     std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+    char e = 'y';
+    char f = 'z';
+    ::swap(e, f);
+    std::cout << "e = " << e << ", f = " << f << std::endl;
+    std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+    std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+    double g = 1.42;
+    double h = 1.43;
+    ::swap(g, h);
+    std::cout << "g = " << g << ", h = " << h << std::endl;
+    std::cout << "min( g, h ) = " << ::min( g, h ) << std::endl;
+    std::cout << "max( g, h ) = " << ::max( g, h ) << std::endl;
     return 0;
 }
